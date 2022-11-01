ee33b7f4dc132dc2ea01cfbe738126b3182aeb10
koutsosg <g.koutsos123@gmail.com>
2022-11-01 20:29:16 +0100

    7/8 pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react"
+import "./App.css";
+import ReactMarkdown from "react-markdown"
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const defaultEditor = `
+# Heading
+
+## Heading 2
+
+This is a paragraph
+
+**Bold text**
+
+- list item
+
+[Visit my Assignment](koutsosg.github.io/markdown-previewer)
+
+this is an inline \`<div></div>\`
+
+This is a codeblock
+\`\`\`python
+print("Hello World")
+\`\`\`
+
+![Markdown](https://cdn1.iconfinder.com/data/icons/logos-and-brands-3/512/205_Markdown_logo_logos-512.png)
+
+> Block Quotes!
+  `;
+  const [container, setContainer] = useState(defaultEditor);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <textarea
+          id="editor"
+          value={container}
+          onChange={(e) => setContainer(e.target.value)}
+          rows="10 "
+          cols="50"
+        ></textarea>
+      </div>
+      <div className="row" id="preview">
+        <ReactMarkdown children={container} />
+      </div>
     </div>
   );
 }
